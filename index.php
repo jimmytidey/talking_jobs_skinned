671fd2ead9f2c80ed8256eb96c22adae7957adc3
Simon Hamilton <simon@seven6eight.com>
2014-04-04 11:36:07 +0100

    Survey Monkey
    Added feedback tab and embedded survey form

--- index.php
+++ index.php
@@ -161,6 +161,15 @@
 	                <div class='question_alert' ></div>
 				</div>
             </div>
+
+
+            <div class='tab tab_feedback'>
+                <h1>Feedback</h1>
+				<div class='content-container'>
+					<script src="https://www.surveymonkey.com/jsEmbed.aspx?sm=LhTuecOlTe2M96wNN3HL2Q_3d_3d"> </script>
+				</div>
+            </div>
+
             
         </div>
         
@@ -172,7 +181,8 @@
                     <li><a href="#reflections">Reflections</a></li>
                     <li><a href="#have_a_quesiton">Ask a question</a></li> 
                     <li><a href="#activities">Activities</a></li> 
-					<li><a href="#about">About</a></li>                     
+					<li><a href="#about">About</a></li>
+					<li><a href="#feedback">Feedback</a></li>                     
                 </ul>
             </nav>
 
