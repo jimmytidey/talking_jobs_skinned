ed00d99a6ffa3e218464f753e4997afc04859069
Jimmy Tidey <jimmytidey@gmail.com>
2014-06-09 22:15:09 +0100

    adding indexOf for ie8

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,26 @@
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 
 job_player.getObjectFromPlaylist = function(interviewee_no, question_no, player_instance){ 
     //find the next playlist item with question ID 
